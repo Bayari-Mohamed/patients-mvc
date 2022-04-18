9fe27b900154a257958c7da74b098d3bed713679
Mohamed Bayari <bayari_mohamed@emsi-edu.ma>
2022-04-18 12:11:21 +0200

    part 3 security

--- src/main/java/ma/emsi/patientsmvc/sec/SecurityConfig.java
+++ src/main/java/ma/emsi/patientsmvc/sec/SecurityConfig.java
@@ -1,5 +1,6 @@
 package ma.emsi.patientsmvc.sec;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,21 +9,27 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 import java.io.PrintStream;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private DataSource dataSource;
    PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
    String encodedPWD = passwordEncoder.encode("1234");
 
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        System.out.println(encodedPWD);
+       /* System.out.println(encodedPWD);
        auth.inMemoryAuthentication().withUser("user1").password(encodedPWD).roles("USER");
-       auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("2345")).roles("ADMIN","USER");
+       auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("2345")).roles("ADMIN","USER");*/
+   auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery(" select username as principal, password as credentials, active from users where username=?").authoritiesByUsernameQuery("select username as principal , role as role from users_roles where username=?").rolePrefix("ROLE_").passwordEncoder(passwordEncoder);
+
     }
 
     @Override
